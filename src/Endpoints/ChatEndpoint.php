feff1c110bda86d391ac8fbb35d1a3e4a9dd9e8d
Filippo Toso <filippo.toso@creativepark.it>
2023-07-26 10:18:30 +0200

    Cleanup

--- src/Endpoints/ChatEndpoint.php
+++ src/Endpoints/ChatEndpoint.php
@@ -11,7 +11,7 @@ class ChatEndpoint extends Endpoint
      * @see https://platform.openai.com/docs/api-reference/chat/create
      * @return Response
      */
-    public function create(string $model, array $message, float $temperature = 1, float $top_p = 1, int $n = 1, bool $stream = false, int $max_tokens = null, float $presence_penalty = 0, float $frequency_penalty = 0, mixed $logit_bias = null, string $user = null): Response
+    public function create(string $model, array $messages, float $temperature = 1, float $top_p = 1, int $n = 1, bool $stream = false, int $max_tokens = null, float $presence_penalty = 0, float $frequency_penalty = 0, mixed $logit_bias = null, string $user = null): Response
     {
         return $this->post('/chat/completions', [], $this->payload(__METHOD__, func_get_args()));
     }
